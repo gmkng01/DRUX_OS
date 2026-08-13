9e6c2ba74db6cfc3d85aba4637cb3bd017a176a9
Abhishek Mishra <gmkng1@gmail.com>
2026-08-13 16:25:01 +0530

    Fixed NVIM errors

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,15 @@
 return {
-	"nvim-treesitter/nvim-treesitter", 
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
 	build = ":TSUpdate",
 	config = function()
 		vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>')
 		local config = require("nvim-treesitter.configs")
 		config.setup({
 			auto_install = true,
-			ensure_installed ={"lua", "python", "bash", "go"},
+			ensure_installed = { "lua", "python", "bash", "go" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
 	end
 }
-
